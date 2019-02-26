4b86be1084e118d19fab06d6f7fa7bc5df23232f
yishuailiu <43007048+yishuailiu@users.noreply.github.com>
2019-02-26 16:16:52 -0700

    asp.net

---
+++ BasicASPX/WebApp/SamplePages/FirstPage.aspx
@@ -0,0 +1,9 @@
+﻿<%@ Page Title="FirstPageHello" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="FirstPage.aspx.cs" Inherits="WebApp.SamplePages.FirstPage" %> 
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <asp:Label ID="Label1" runat="server" Text="Enter your Name"></asp:Label>&nbsp;&nbsp;
+    <asp:TextBox ID="YourName" runat="server" OnTextChanged="TextBox1_TextChanged"></asp:TextBox>
+    <br />
+    <asp:Button ID="PressMe" runat="server" Text="Press Me" OnClick="PressMe_Click" />
+    <br />
+    <asp:Literal ID="OutputMessage" runat="server"></asp:Literal>
+</asp:Content>
---
+++ BasicASPX/WebApp/SamplePages/FirstPage.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApp.SamplePages
+{
+    public partial class FirstPage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //this method will execute EACH and EVERY time this page is processed
+        }
+
+        protected void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+            //this method will exectue BEFORE ANY event method
+        }
+
+        protected void PressMe_Click(object sender, EventArgs e)
+        {
+            OutputMessage.Text ="hello " + YourName.Text;
+        }
+    }
+}
